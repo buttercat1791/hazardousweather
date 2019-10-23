eb99876362831f746da355ea51e990b3ad1032d2
Michael <michaelnotbolton@gmail.com>
2019-10-23 11:44:59 -0500

    Added Keys
    Consumer keys and access tokens added to both bots

--- trueconfig.js
+++ trueconfig.js
@@ -1,7 +1,7 @@
 // Twitter application info for the true bot goes here
 module.exports = {
-    consumer_key: '',
-    consumer_secret: '',
-    access_token_key: '',
-    access_token_secret: '',
+    consumer_key: 'Vwbx6Cieo97iAhd5emwyDBzNx',
+    consumer_secret: 'v0qY0VEGrW5UPW33HZRnyRitXTgRr2Ijt2byZiV1VBnIcHaCQk',
+    access_token_key: '844427580086386688-EvTuAVhn0qzac6LIvljwWFnN9NzK4fi',
+    access_token_secret: 'hsjNNdO1nkGaJXuvQLbOsE90m5CRyM1cSqziLwAPFY9bq',
 }
