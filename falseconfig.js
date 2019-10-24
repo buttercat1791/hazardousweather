c1c07d0386af3f81a72102b406d3a48125ede13e
Michael <michaelnotbolton@gmail.com>
2019-10-24 09:24:05 -0500

    Security Hotfix
    Depricated Keys
    Cleared Depricated Keys

--- falseconfig.js
+++ falseconfig.js
@@ -1,7 +1,7 @@
 // Twitter application info for the false bot goes here
 module.exports = {
-  consumer_key: 'K8lvudZ0tQpFK4qbzikfSCCG8',
-  consumer_secret: 'OYa88fs5pDuCA0ZZ5gENlAqgDPBO32q2ZrZnJC7e82XGUymyEi',
-  access_token_key: '844427580086386688-1a9UI8bDLkXcu6XaBPC6bBdjq2atYyH',
-  access_token_secret: 'PlcdQ9nCB0p6Iyq3gN3AmDfEPQjvm3qhMXH9d5CAuVjhN',
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: '',
 }
